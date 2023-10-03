e6b7fd9037a9e9b60c66c4e046da1a3c0295ce12
Tetardtek <kvnn64@gmail.com>
2023-10-03 11:12:27 +0200

    First stage brut color

---
+++ index.mjs
@@ -0,0 +1,17 @@
+const names = ["Paul", "Benoît", "Christopher", "Sandrine", "Cédric"];
+// const chalk = require("chalk");
+import chalk from 'chalk';
+
+
+console.log(
+    chalk.red(`Hello ${names[0]}`)
+);
+console.log(
+    chalk.yellow(`Hello ${names[1]}`)
+);
+console.log(
+    chalk.green(`Hello ${names[2]}`)
+);
+console.log(
+    chalk.blue(`Hello ${names[3]}`)
+);
