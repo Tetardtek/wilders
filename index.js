4fc0528b83c6b7652ab1f49cf1769e937d2db582
Tetardtek <kvnn64@gmail.com>
2023-10-03 11:49:54 +0200

    type Module

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
+// let randomValue = names[Math.floor(Math.random() * names.length)];
+// const chalk = require("chalk");
+import chalk from 'chalk';
+const names = ["Paul", "Benoît", "Christopher", "Sandrine", "Cédric", "Suzy", "Alhassane", "Didier", "Xavier"];
+
 function randomValue(names)
 {
     return names[Math.floor(Math.random() * names.length)];
 }
-// let randomValue = names[Math.floor(Math.random() * names.length)];
-const names = ["Paul", "Benoît", "Christopher", "Sandrine", "Cédric", "Suzy", "Alhassane", "Didier", "Xavier"];
-// const chalk = require("chalk");
-import chalk from 'chalk';
-
 console.log(
     chalk.red(`Hello ${(randomValue(names))}`)
 );
