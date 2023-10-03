c868af82f2b680b373b96f508be969a9d2ccc41e
Tetardtek <kvnn64@gmail.com>
2023-10-03 11:35:52 +0200

    add randomize array

--- index.mjs
+++ index.mjs
@@ -1,17 +1,21 @@
-const names = ["Paul", "Benoît", "Christopher", "Sandrine", "Cédric"];
+function randomValue(names)
+{
+    return names[Math.floor(Math.random() * names.length)];
+}
+// let randomValue = names[Math.floor(Math.random() * names.length)];
+const names = ["Paul", "Benoît", "Christopher", "Sandrine", "Cédric", "Suzy", "Alhassane", "Didier", "Xavier"];
 // const chalk = require("chalk");
 import chalk from 'chalk';
 
-
 console.log(
-    chalk.red(`Hello ${names[0]}`)
+    chalk.red(`Hello ${(randomValue(names))}`)
 );
 console.log(
-    chalk.yellow(`Hello ${names[1]}`)
+    chalk.yellow(`Hello ${(randomValue(names))}`)
 );
 console.log(
-    chalk.green(`Hello ${names[2]}`)
+    chalk.green(`Hello ${(randomValue(names))}`)
 );
 console.log(
-    chalk.blue(`Hello ${names[3]}`)
+    chalk.blue(`Hello ${(randomValue(names))}`)
 );
